e61f19f5fe35faaa2afe0e960273aecb8266680b
Radian06 <aksen05aksen@gmail.com>
2025-11-25 23:39:31 +0900

    📄 :: home 페이지 생성 및 환경 설정

---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import "../styles/home.css"
+
+function Home() {
+  return (
+    <div className="home_page">
+      <div className="home_title">Truth Lens</div>
+    </div>
+  );
+}
+
+export default Home;
