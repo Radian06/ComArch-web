36ceb39698c89856be8186398ff5a84aeb69e063
Radian06 <aksen05aksen@gmail.com>
2025-11-26 12:50:02 +0900

    ⚡️ : Footer 추가

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,25 @@
 import "../styles/home.css"
 import { Link } from "react-router-dom";
 import Lens from "../assets/lensImg.png";
+import Footer from "../components/footer.jsx";
 
 function Home() {
   return (
     <div className="home_page">
-      <div className="home_leftBox">
+      <div className="home_wholeBox">
         <img src={Lens} className="home_lens" />
-      </div>
-
-      <div className="home_rightBox">
-        <div className="home_title">Truth Lens</div>
-        <div className="home_subTitle">세상을 보는 눈, 더 이상 AI가 쓴 가짜 뉴스에 속지 마세요.</div>
-        <div className="home_start">시작하기</div>
-        <div className="home_account_box">
-          <Link to = '/join' className="home_account">회원가입</Link>
-          /
-          <Link to = '/login' className="home_account">로그인</Link>
+        <div className="home_rightBox">
+          <div className="home_title">Truth Lens</div>
+          <div className="home_subTitle">세상을 보는 눈, 더 이상 AI가 쓴 가짜 뉴스에 속지 마세요.</div>
+          <div className="home_start">시작하기</div>
+          <div className="home_account_box">
+            <Link to = '/join' className="home_account">회원가입</Link>
+            /
+            <Link to = '/login' className="home_account">로그인</Link>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
