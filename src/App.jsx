712a64c7f36919db743517425aa5e6eb5685b5d7
Radian06 <aksen05aksen@gmail.com>
2025-11-26 16:43:29 +0900

    📄 :: Inspection 페이지 생성"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
+import Inspection from "./pages/inspection.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/inspection" element={<Inspection />} />
     </Routes>
   );
 }
---
+++ src/pages/inspection.jsx
@@ -0,0 +1,14 @@
+import "../styles/inspection.css"
+
+import Footer from "../components/footer.jsx";
+
+function Inspection() {
+  return (
+    <div className="home_page">
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default Inspection;
