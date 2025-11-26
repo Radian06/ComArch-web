593f7a55feb472aea81844f053592911ef64c5b3
Radian06 <aksen05aksen@gmail.com>
2025-11-26 12:49:35 +0900

    ⚡️ ::Footer 퍼블리싱

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+import "../styles/footer.css"
+
+function Footer() {
+  return (
+    <div className="footer_page">
+        <div className="footer_nav">인터넷과 멀티미디어 통신   |   국립경국대학교 소프트웨어융합학과</div>
+    </div>
+  );
+}
+
+export default Footer;
