c708d8ea8036c311524069220c1c75c70075a855
Radian06 <aksen05aksen@gmail.com>
2025-12-01 15:08:48 +0900

    📄 :: result 페이지 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Inspection from "./pages/inspection.jsx";
+import Result from "./pages/result.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/inspection" element={<Inspection />} />
+      <Route path="/result" element={<Result />} />
     </Routes>
   );
 }
---
+++ src/pages/result.jsx
@@ -0,0 +1,14 @@
+import "../styles/result.css"
+import "../styles/headerWhole.css"
+
+import Footer from "../components/footer.jsx";
+
+function Result() {
+  return (
+    <div className="result_page">
+      <Footer />
+    </div>
+  );
+}
+
+export default Result;
