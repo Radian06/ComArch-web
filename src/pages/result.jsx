22c1d93cbb68ff6abc8b1b80d93bda89657fce64
Radian06 <aksen05aksen@gmail.com>
2025-12-01 15:52:13 +0900

    ⚡️: result 페이지 퍼블리싱

--- src/pages/result.jsx
+++ src/pages/result.jsx
@@ -6,6 +6,19 @@ import Footer from "../components/footer.jsx";
 function Result() {
   return (
     <div className="result_page">
+        <div className="header">
+            <div className="back_btn">시작 화면으로 돌아가기</div>
+            <div className="nav_box">
+                <div className="nav_select">검사하기</div>
+                <div>|</div>
+                <div>사용기록</div>
+            </div>
+        </div>
+        <div className="result_body">
+            <div className="result_title">가짜 입니다.</div>
+            <div className="default_btn">의심 확률: n%</div>
+            <div className="result_container"></div>
+        </div>
       <Footer />
     </div>
   );
