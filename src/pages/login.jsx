14644510bca848265f69c0af7b1f95876e4b1d60
Radian06 <aksen05aksen@gmail.com>
2025-12-02 13:56:14 +0900

    ⚡️: login join css account로 통합 & login 페이지 퍼블리싱

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,19 +1,26 @@
-import "../styles/login.css"
+import "../styles/account.css"
 import "../styles/headerWhole.css"
-
+import { Link } from "react-router-dom";
 import Footer from "../components/footer.jsx";
 
 function Login() {
   return (
-    <div className="login_page">
-      <div className="header">
+    <div className="account_page">
+        <div className="header">
         <div className="back_btn">시작 화면으로 돌아가기</div>
-
-      </div>
-      <div className="login_body">
-       
-      </div>
-      <Footer />
+    </div>
+    <div className="account_body">
+        <div className="account_title">로그인</div>
+        <input className="account_input" placeholder="아이디 또는 이메일"></input>
+        <input className="account_input" placeholder="비밀번호"></input>
+        <div className="default_btn">확인</div>
+        <div className="account_box">
+            <Link to = '/join' className="account_nav">회원가입</Link>
+            <div className="account_nav">/</div>
+            <div className="account_nav">계정찾기</div>
+        </div>
+    </div>
+    <Footer />
     </div>
   );
 }
